42deabd9e4a5e3c72f5f032261e5cd0475188511
Dmytro <dimon2000.1982@gmail.com>
2020-03-29 23:30:21 +0300

    На основі алгоритма Дейкстри реалізував пошук найкоротшого шляху
    Поки що програма видає тільки список пройдених вершин і відстань до них, далі слід реалізувати функцію для відновлення найкоротшого шляху  і запису його в лабіринт

--- OP_lab3_VAR1.py
+++ OP_lab3_VAR1.py
@@ -17,6 +17,38 @@ def output_matrix(matrix):
 			print("{:}".format(n), end=" ")
 		print() 
 
+def dijkstra(lab,start = [],end = []):
+	perm = [start+[0]]
+	temp = []
+	flag = False
+	l = 0
+	cointer = 1
+	for i in perm:
+		temp =[i[0]-1,i[1],cointer], [i[0]+1,i[1],cointer], [i[0],i[1]-1,cointer], [i[0],i[1]+1,cointer]
+
+		temp1 = []
+		print(temp)
+		for k in temp:
+			if lab[k[0]][k[1]] == ' ':
+				temp1.append(k)
+			if k[:-1] == end:
+				flag = True
+			for j in range(len(perm)):
+				if k[:-1] == perm[j][:-1] and k[-1] > perm[j][-1]:
+					if k in temp1:
+						temp1.remove(k)
+		print(temp1)
+
+		if temp1:
+			cointer +=1
+		perm.extend(temp1)
+		print(perm)
+		if flag:
+			break
+	print("Final:\n")
+	print(perm)
+
 def main():
-	lab = opener('lab.txt')
+	lab = opener('lab2.txt')
+	dijkstra(lab,start = [5,1],end= [1,14])
 main()
